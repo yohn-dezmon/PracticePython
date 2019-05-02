bedb20e4ab7403ae8f20e56d4fb3bad1d5911a85
Johnboi <johndesmond631@gmail.com>
2019-05-02 12:55:04 -0500

    completed ex5 by putting the functionality into one line using set and list()

--- ex5.py
+++ ex5.py
@@ -39,8 +39,9 @@ check_for_dupes(aa, bb)
 
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-# 
-# c = []
-# z = [c.append(x) for x in b if x in b and x not in c]
 #
-# print(z)
+# the {} here indicate a set, a set consists of only unique values! :D 
+c = list({x for x in a if x in b})
+#
+print(c)
+print(c[3])
